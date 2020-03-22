91a06279da92cf6562dcc04514097c92a0d03eed
abir <abir4u2011@gmail.com>
2020-03-22 21:25:55 +0600

    scraping

---
+++ webScraping/mapIt.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+# take an address as input could be anything like "ruhitpur"
+address = input('enter the address: ')
+webbrowser.open('http://www.google.com/maps/place/'+address)
---
+++ webScraping/webscrapingpractice.py
@@ -0,0 +1,3 @@
+import webbrowser
+
+webbrowser.open('https://inventwithpython.com/')
